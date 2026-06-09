32c39ed38d71eeed35752238dd03682dd31a896a
paul-murithi <paulmurithi518@gmail.com>
2026-06-09 08:45:17 +0300

    feat: SKU dynamic generation in product factory for tests

--- tests/factories.py
+++ tests/factories.py
@@ -9,8 +9,9 @@ class ProductFactory(factory.Factory):
 
     name = factory.Faker("word")
     price = factory.Faker("pydecimal", left_digits=4, right_digits=2, positive=True)
+    sku = factory.Sequence(lambda n: f"SKU-{n + 1:06d}")
 
     @classmethod
     def as_dict(cls, **kwargs):
         obj = cls.build(**kwargs)
-        return {"name": obj.name, "price": obj.price}
+        return {"name": obj.name, "price": obj.price, "sku": obj.sku}
