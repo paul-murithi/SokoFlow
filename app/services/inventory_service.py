f48398e50a17391cf8d1832f51a42dc9ba6573f3
paul-murithi <paulmurithi518@gmail.com>
2026-06-15 08:29:59 +0300

    chore: Inventory service template

---
+++ app/services/inventory_service.py
@@ -0,0 +1,15 @@
+class InventoryService:
+    def add_stock(self) -> None:
+        pass
+
+    def remove_stock(self) -> None:
+        pass
+
+    def get_stock(self) -> None:
+        pass
+
+    def is_low_stock(self) -> None:
+        pass
+
+    def update_threshold(self) -> None:
+        pass
