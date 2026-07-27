9d81e76aa77c2b235492a97661124034dd9a42e6
paul-murithi <paulmurithi518@gmail.com>
2026-07-27 09:03:38 +0300

    feat: Define fsm session model - implemented a native StrEnum to act as a strict check for session states - prepared schema for stateless redis and celery pipelines

--- app/utils/types.py
+++ app/utils/types.py
@@ -1,10 +1,42 @@
+from datetime import datetime
 from enum import StrEnum
+from typing import List
+
+from pydantic import BaseModel
 
 
 class QueueName(StrEnum):
+    """
+    Celery Worker Queue names
+    """
+
     CONVERSATION = "conversation_tasks"
     REPORTS = "reports_tasks"
     NOTIFICATIONS = "notifications_tasks"
     MAINTENANCE = "maintenance_tasks"
     DEFAULT = "default_tasks"
     SCHEDULES = "schedules_tasks"
+
+
+# FSM Redis Sessions
+class SessionState(StrEnum):
+    IDLE = "IDLE"
+    SALE = "SALE"
+    WAIT_PRODUCT = "RECORD_SALE_PRODUCT"
+    WAIT_QTY = "RECORD_SALE_QTY"
+    CONFIRM = "CONFIRM_SALE"
+    DONE = "DONE"
+
+
+class SessionContext(BaseModel):
+    product_name: str
+    flow_started_at: datetime
+    history: List[SessionState]
+    error_count: int
+    last_activity: datetime
+
+
+class UserSession(BaseModel):
+    phone: str
+    state: SessionState
+    context: SessionContext
